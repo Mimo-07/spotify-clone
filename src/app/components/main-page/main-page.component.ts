1ff5b7f8a23f1d59f7b420455909297826afe320
Bijit Pal Chaudhuri <Bijitpalchaudhuri@gmail.com>
2025-12-20 00:41:24 +0530

    chore: remove unwanted code

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -25,42 +25,5 @@ export class MainPageComponent implements OnInit {
     },
   ];
 
-  ngOnInit(): void {
-    // const previousState = localStorage.getItem('auth_state');
-    // const previousVerifier = localStorage.getItem('code_verifier');
-    // const returnedData = new URLSearchParams(window.location.search);
-    // const returnedState = returnedData.get('state');
-    // const returnedCode = returnedData.get('code');
-    // if (previousState && previousVerifier && returnedState && returnedCode) {
-    //   if (returnedState != previousState) {
-    //     this.#router.navigate(['login']);
-    //   } else {
-    //     this.#tokenService.getToken(returnedCode, previousVerifier).subscribe({
-    //       next: (response: LoginResponse) => {
-    //         if (response.access_token) {
-    //           sessionStorage.setItem(
-    //             TokenType.ACCESS_TOKEN,
-    //             response.access_token,
-    //           );
-    //         }
-    //         if (response.refresh_token) {
-    //           sessionStorage.setItem(
-    //             TokenType.REFRESH_TOKEN,
-    //             response.refresh_token,
-    //           );
-    //         }
-    //         this.#router
-    //           .navigate(['/'], {
-    //             queryParams: {},
-    //             onSameUrlNavigation: 'reload',
-    //             skipLocationChange: true,
-    //           })
-    //           .then(() => {
-    //             this.#router.navigate(['/home']);
-    //           });
-    //       },
-    //     });
-    //   }
-    // }
-  }
+  ngOnInit(): void {}
 }
