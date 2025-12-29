aa715aa5366a77fba2427f5cd126b9ec52a5c0f6
Bijit Pal Chaudhuri <Bijitpalchaudhuri@gmail.com>
2025-12-29 12:18:49 +0530

    chore: remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CdkDragMove } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -8,28 +7,4 @@ import { CdkDragMove } from '@angular/cdk/drag-drop';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  #defaultWidth = 250;
-
-  currentWidthLeft = signal(this.#defaultWidth);
-  currentWidthRight = signal(this.#defaultWidth);
-
-  ngOnInit(): void {}
-
-  onDragMoved(resizeEvent: CdkDragMove, resizedPane: string) {
-    switch (resizedPane) {
-      case 'left':
-        this.currentWidthLeft.set(resizeEvent.pointerPosition.x);
-        break;
-      case 'right':
-        const container = document.querySelector('.container') as HTMLElement;
-        const containerWidth = container.offsetWidth;
-        this.currentWidthRight.set(
-          containerWidth - resizeEvent.pointerPosition.x,
-        );
-        break;
-    }
-    const element = resizeEvent.source.element.nativeElement;
-    element.style.transform = 'none';
-  }
-}
+export class AppComponent {}
